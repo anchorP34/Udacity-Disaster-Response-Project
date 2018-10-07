e83d7035ca6b503252725cfa06f68fb81b0c7fac
Payton Soicher <42127929+anchorP34@users.noreply.github.com>
2018-10-07 09:00:11 -0600

    ETL of Messages and Categories

--- process_data.py
+++ process_data.py
@@ -6,6 +6,15 @@ from sqlalchemy import create_engine
 
 # Load in the messages and the affiliated categories
 def load_data(messages_filepath, categories_filepath):
+    """
+    Load data from a database
+    Input: messages_filepath - String
+            The path to the messagages data csv
+           categories_filepath - String
+            The path to the categories data csv
+    Output: df = DataFrame
+            Merged DataFrame of message and categories data
+    """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     # Merge the two datasets together
@@ -15,6 +24,13 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
+    """
+    Extracts categories and flags from categories data, remove duplicates
+    Input: df - DataFrame
+            Dataframe output from load_data function
+    Output: df - DataFrame
+            Cleansed dataframe of the input data
+    """
     # Get the categories column from the DF, then find the strings attached
     categories = df['categories'].str.split(n = 36, pat = ';', expand = True)
     row = categories.iloc[0,:]
@@ -44,12 +60,24 @@ def clean_data(df):
 
 
 def save_data(df, database_filename, table_name = 'StagingMLTable'):
+    """
+    Save data to database
+    Input: df - DataFrame
+            DataFrame from clean_data dataframe
+           database_filename - String
+           Database file location of where data is to be stored
+           table_name - String (default is StagingMLTable)
+           The name of the table that the data should be saved to 
+    """
     engine = create_engine('sqlite:///'+database_filename)
     df.to_sql(table_name, engine, index=False)  
     print("Data was saved to {} in the {} table".format(database_filename, table_name))
 
 
 def main():
+    """
+    Run ETL of messages and categories data
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
